cbb15e15440d7482f132a5d71e51e49261417f88
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-06 16:09:26 +0200

    Update Survey.java

--- src/main/java/com/ennajim/surveyms/entities/Survey.java
+++ src/main/java/com/ennajim/surveyms/entities/Survey.java
@@ -4,8 +4,10 @@ package com.ennajim.surveyms.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -16,14 +18,14 @@ public class Survey {
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         private Long id;
-
-     /*   @ManyToMany
+        private String title ;
+        private String description ;
+   @ManyToMany
         @JoinTable(
                 name = "survey_question",
                 joinColumns = @JoinColumn(name = "survey_id"),
                 inverseJoinColumns = @JoinColumn(name = "question_id"))
         private List<Question> questions;
 
-*/
 }
 
