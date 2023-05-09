bfeaf85430ae5f9b0e2f4eaca8a656cb829f0989
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-09 16:51:11 +0200

    Update SurveyRepository.java

--- src/main/java/com/ennajim/surveyms/repository/SurveyRepository.java
+++ src/main/java/com/ennajim/surveyms/repository/SurveyRepository.java
@@ -5,10 +5,12 @@ import com.ennajim.surveyms.entities.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey,Long> {
-/*
-    List<Survey> findByFieldContains(String field);
-*/
+
+    Survey findByField(String field);
+
 
 }
