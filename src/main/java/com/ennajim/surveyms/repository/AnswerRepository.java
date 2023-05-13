6bf63f006dd478e36aebd8aa21d5d3881ad52d43
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-13 21:27:35 +0200

    Update AnswerRepository.java

--- src/main/java/com/ennajim/surveyms/repository/AnswerRepository.java
+++ src/main/java/com/ennajim/surveyms/repository/AnswerRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+
+/*
+    void FindByAnswer(Answer answer);
+*/
 }
